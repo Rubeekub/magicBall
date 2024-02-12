523ff1778f05fa7ca29264677da995076bfacafa
Rubeekub <134529862+Rubeekub@users.noreply.github.com>
2024-02-12 10:37:55 +0100

    Update footer.php
    suppression balise head sur footer

--- footer.php
+++ footer.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-</head>
-<body>
-    
-</body>
 <footer>
     <div class="content">La seule reponse possible et vrai sera 42!</div>
 </footer>
