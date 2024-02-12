926885dee3a38b02adf96bec2fd607ecc0decc9c
Rubeekub <134529862+Rubeekub@users.noreply.github.com>
2024-02-12 10:36:55 +0100

    Update index.php
    suppression lignes vides

--- index.php
+++ index.php
@@ -1,35 +1,18 @@
 <?php 
     include('header.php'); 
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-</head>
 <body>
-    
-
 <div class="container">
             <div class="content">
                 <div class="title" title="Magic Ball">Magic Ball</div>
-     
                 <div class="question utilisateur"><br />
                 <form action="action.php" method="POST">
                     <label for="question">Quelle est votre question?</label>
                     <input name="question" id="question" type="text" />
-                    
                     <p><button type="submit">Valider</button></p>
                 </form>
                 </div>
-               
             </div>
-
         </div>
-            
         </body>
-        ><footer>
-            
-        </footer>
 </html>
