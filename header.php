7fbf99de936d8b4df736acafdcd3387dd8794332
Rubeekub <134529862+Rubeekub@users.noreply.github.com>
2024-02-12 10:35:06 +0100

    Update header.php
    suppression des lignes vides

--- header.php
+++ header.php
@@ -9,7 +9,6 @@
             html, body {
                 height: 100%;
             }
-
             body {
                 margin: 0;
                 padding: 0;
@@ -18,22 +17,18 @@
                 font-weight: 100;
                 font-family: 'Karla';
             }
-
             .container {
                 text-align: center;
                 display: table-cell;
                 vertical-align: middle;
             }
-
             .content {
                 text-align: center;
                 display: inline-block;
             }
-
             .title {
                 font-size: 96px;
             }
-
         </style>
     </head>
     <body>
